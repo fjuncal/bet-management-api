ee2b89aa55f248e106cb943cc8a39faf95f37479
Fellipe Juncal <fjuncal7@gmail.com>
2024-09-08 00:32:52 -0300

    Initial commit

---
+++ src/main/java/com/betmanager/BetManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.betmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/betmanager/models/User.java
@@ -0,0 +1,32 @@
+package com.betmanager.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String role;
+}
+
+
+
