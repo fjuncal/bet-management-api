7edf229834833ed5758c90585411d83e4dbbd1a9
Fellipe Juncal <fjuncal7@gmail.com>
2024-09-09 17:23:29 -0300

    Adicionando JWT Token no projeto.

---
+++ src/main/java/com/betmanager/security/JwtRequestFilter.java
@@ -0,0 +1,27 @@
+package com.betmanager.security;
+
+import com.betmanager.services.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//O filtro JWT intercepta todas as requisições e valida o token JWT:
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
