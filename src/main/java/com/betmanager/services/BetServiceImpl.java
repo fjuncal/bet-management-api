530caad6cba20718494d05b38877d7b5c2892c3e
Fellipe Juncal <fjuncal7@gmail.com>
2024-09-11 17:47:01 -0300

    Adicionando visualização de log na aplicação

--- src/main/java/com/betmanager/services/BetServiceImpl.java
+++ src/main/java/com/betmanager/services/BetServiceImpl.java
@@ -9,6 +9,8 @@ import com.betmanager.repositories.UserRepository;
 import com.betmanager.repositories.specification.BetSpecifications;
 import com.betmanager.services.interfaces.IBetService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +23,14 @@ import java.util.List;
 public class BetServiceImpl implements IBetService {
     private final BetRepository betRepository;
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(BetServiceImpl.class);
+
     @Override
     public Bet saveBet(Bet bet, Long userId) {
         UserEntity userEntity = userRepository.findById(userId)
                 .orElseThrow(() -> new NoUserFoundException("User not found"));
         bet.setUser(userEntity);
+        logger.info("User {} created a bet with ID {}", userId, bet.getId());
         return betRepository.save(bet);
     }
 
@@ -43,6 +48,7 @@ public class BetServiceImpl implements IBetService {
         bet.setStatus(betDetails.getStatus());
         bet.setOdds(betDetails.getOdds());
 
+        logger.info("User {} updated a bet with ID {}", bet.getUser().getId(), bet.getId());
         return betRepository.save(bet);
     }
 
@@ -51,6 +57,7 @@ public class BetServiceImpl implements IBetService {
         Bet bet = betRepository.findById(betId)
                 .orElseThrow(() -> new NoBetFoundException("Bet not found"));
 
+        logger.info("User {} deleted a bet with ID {}", bet.getUser().getId(), betId);
         betRepository.delete(bet);
     }
 
