351b068ad9109b4fa5e98d88be7e6b33f5f4467e
Fellipe Juncal <fjuncal7@gmail.com>
2024-09-30 20:24:03 -0300

    Adicionando Hateoas no projeto e implementando uma melhor maneira de retornar uma página

--- src/main/java/com/betmanager/interfaces/rest/IBetAPI.java
+++ src/main/java/com/betmanager/interfaces/rest/IBetAPI.java
@@ -2,8 +2,10 @@ package com.betmanager.interfaces.rest;
 
 import com.betmanager.models.entities.Bet;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +18,7 @@ public interface IBetAPI {
     ResponseEntity<Bet> createBet(@Valid @RequestBody Bet bet, @PathVariable Long userId);
 
     @GetMapping("/user/{userId}")
-    ResponseEntity<Page<Bet>> getBetsByUserId(@PathVariable Long userId, Pageable pageable);
+    ResponseEntity<PagedModel<EntityModel<Bet>>> getBetsByUserId(@PathVariable Long userId, Pageable pageable, PagedResourcesAssembler<Bet> assembler);
 
     @PutMapping("/{betId}")
     ResponseEntity<Bet> updateBet(@PathVariable Long betId, @RequestBody Bet betDetails);
@@ -25,12 +27,13 @@ public interface IBetAPI {
     ResponseEntity<Void> deleteBet(@PathVariable Long betId);
 
     @GetMapping("/getByFilters")
-    ResponseEntity<Page<Bet>> getBetsByFilters(
+    ResponseEntity<PagedModel<EntityModel<Bet>>> getBetsByFilters(
             @RequestParam(required = false) LocalDate startDate,
             @RequestParam(required = false) LocalDate endDate,
             @RequestParam(required = false) String status,
             @RequestParam(required = false) BigDecimal minAmount,
             @RequestParam(required = false) BigDecimal maxAmount,
             @RequestParam Long userId,
-            Pageable pageable); //userId obrigatorio
+            Pageable pageable,
+            PagedResourcesAssembler<Bet> assembler); //userId obrigatorio
 }
--- src/main/java/com/betmanager/interfaces/rest/controllers/BetController.java
+++ src/main/java/com/betmanager/interfaces/rest/controllers/BetController.java
@@ -10,14 +10,18 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -38,9 +42,9 @@ public class BetController implements IBetAPI {
 
     @Override
     @RateLimiter(name = "defaultRateLimiter", fallbackMethod = "rateLimitFallback")
-    public ResponseEntity<Page<Bet>> getBetsByUserId(@PathVariable Long userId, Pageable pageable) {
+    public ResponseEntity<PagedModel<EntityModel<Bet>>> getBetsByUserId(@PathVariable Long userId, Pageable pageable, PagedResourcesAssembler<Bet> assembler) {
         Page<Bet>bets = betService.getBetsByUserId(userId, pageable);
-        return ResponseEntity.ok(bets);
+        return ResponseEntity.ok(assembler.toModel(bets));
     }
     public ResponseEntity<Page<Bet>> rateLimitFallback(Long userId, Pageable pageable, Throwable t) {
         return ResponseEntity.status(429).body(Page.empty());
@@ -59,9 +63,9 @@ public class BetController implements IBetAPI {
     }
 
     @Override
-    public ResponseEntity<Page<Bet>> getBetsByFilters(LocalDate startDate, LocalDate endDate, String status, BigDecimal minAmount, BigDecimal maxAmount, Long userId, Pageable pageable) {
+    public ResponseEntity<PagedModel<EntityModel<Bet>>> getBetsByFilters(LocalDate startDate, LocalDate endDate, String status, BigDecimal minAmount, BigDecimal maxAmount, Long userId, Pageable pageable, PagedResourcesAssembler<Bet> assembler) {
         Page<Bet> report = betService.getBetsByFilters(startDate, endDate, status, minAmount, maxAmount, userId, pageable);
-        return ResponseEntity.ok(report);
+        return ResponseEntity.ok(assembler.toModel(report));
     }
 
 
